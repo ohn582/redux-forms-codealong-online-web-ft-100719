4f2e8047a8a6bb692538013a550dbde6b73f20ab
Neal JIn Oh <no4259@gmail.com>
2020-03-05 00:20:32 +0000

    Automatically backed up by Learn

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -21,7 +21,7 @@ class CreateTodo extends Component {
         <form>
           <p>
             <label>add todo</label>
-            <input type="text" onChange={(event) => this.handleChange(event)} />
+            <input type="text" onChange={this.handleChange} />
           </p>
           <input type="submit" />
         </form>
